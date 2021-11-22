61cf24ae8a830f359092492b41423c0a53eb6b4d
Chris <christopher.brighton@netnatives.co.uk>
2021-11-22 08:19:36 +0000

    formatting

--- scripts/init.js
+++ scripts/init.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 function copyDirectory(source, destination) {
-  fs.mkdirSync(destination, { recursive: true });
+  fs.mkdirSync(destination, { recursive: true })
 
   fs.readdirSync(source, { withFileTypes: true }).forEach((entry) => {
-    let sourcePath = path.join(source, entry.name);
-    let destinationPath = path.join(destination, entry.name);
+    let sourcePath = path.join(source, entry.name)
+    let destinationPath = path.join(destination, entry.name)
 
     entry.isDirectory()
       ? copyDirectory(sourcePath, destinationPath)
-      : fs.copyFileSync(sourcePath, destinationPath);
-  });
+      : fs.copyFileSync(sourcePath, destinationPath)
+  })
 }
 
 copyDirectory(
-  process.env.PWD + "/node_modules/nuxt-serve/scripts/template/init",
+  process.env.PWD + '/node_modules/nuxt-serve/scripts/template/init',
   process.env.PWD
-);
+)
--- scripts/service.js
+++ scripts/service.js
@@ -1,53 +1,53 @@
-const fs = require("fs");
-const { service, model } = require("./template/service");
+const fs = require('fs')
+const { service, model } = require('./template/service')
 
 function checkPath(path) {
-  return fs.existsSync(path);
+  return fs.existsSync(path)
 }
 
 function makeDir(path) {
   if (!checkPath(path)) {
     try {
-      fs.mkdirSync(path);
+      fs.mkdirSync(path)
     } catch (ex) {}
   }
 }
 
 function makeFile(path, name, ext, data) {
   if (checkPath(path)) {
-    const file = `${path}/${name}.${ext}`;
+    const file = `${path}/${name}.${ext}`
     try {
-      fs.writeFileSync(file, data, { encoding: "utf8" });
+      fs.writeFileSync(file, data, { encoding: 'utf8' })
     } catch (ex) {}
   }
 }
 
 const name = {
-  js: "",
-  text: "",
-  db: "",
-};
-
-const unformattedName = process.argv[2];
-
-const words = unformattedName.split(" ").join("").split("-");
-for (let i = 1; i < words.length; i++) {
-  words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  js: '',
+  text: '',
+  db: '',
 }
-name.js = words.join("");
 
-name.text = unformattedName.split(" ").join("");
+const unformattedName = process.argv[2]
 
-name.db = unformattedName.split(" ").join("").split("-").join("_");
+const words = unformattedName.split(' ').join('').split('-')
+for (let i = 1; i < words.length; i++) {
+  words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+}
+name.js = words.join('')
 
-const fields = [];
+name.text = unformattedName.split(' ').join('')
+
+name.db = unformattedName.split(' ').join('').split('-').join('_')
+
+const fields = []
 if (process.argv.length > 3) {
   for (let i = 3; i < process.argv.length; i++) {
-    fields.push(process.argv[i]);
+    fields.push(process.argv[i])
   }
 }
 
-const path = `${process.env.PWD}/server/services/${name.text}`;
-makeDir(path);
-makeFile(path, "index", "js", service(name));
-makeFile(path, "model", "js", model(name));
+const path = `${process.env.PWD}/server/services/${name.text}`
+makeDir(path)
+makeFile(path, 'index', 'js', service(name))
+makeFile(path, 'model', 'js', model(name))
