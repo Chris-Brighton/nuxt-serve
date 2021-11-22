938100a26f99b7c03add42c6b55bb026fed0b9e9
Chris <christopher.brighton@netnatives.co.uk>
2021-11-22 23:46:24 +0000

    add parse

--- scripts/template/init/server/index.js
+++ scripts/template/init/server/index.js
@@ -1,21 +1,24 @@
 const { NuxtServe } = require('nuxt-serve')
 const services = require('./services')
+const Sequelize = require('sequelize')
 
 const nuxtServe = new NuxtServe()
-nuxtServe.sequelize({
-  dialect: 'sqlite',
-  logging: false,
-  storage: `${process.env.PWD}/database.sqlite`,
-  query: {
-    raw: true,
-  },
-  define: {
-    freezeTableName: true,
-    underscored: false,
-    createdAt: true,
-    updatedAt: true,
-  },
-})
+nuxtServe.sequelize(
+  new Sequelize({
+    dialect: 'sqlite',
+    logging: false,
+    storage: `${process.env.PWD}/database.sqlite`,
+    query: {
+      raw: true,
+    },
+    define: {
+      freezeTableName: true,
+      underscored: false,
+      createdAt: true,
+      updatedAt: true,
+    },
+  })
+)
 const app = nuxtServe.app()
 app.configure(services)
 app.start()
