22ce9a15064e5446b36fd05b60a6f278628b7590
Chris <christopher.brighton@netnatives.co.uk>
2022-11-01 13:54:39 +0000

    auth

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const NuxtServe = require('./lib/NuxtServe')
 const Service = require('./lib/Service')
 const UploadService = require('./lib/UploadService')
+const AuthService = require('./lib/auth/AuthService')
 
 module.exports = {
   NuxtServe,
   Service,
   UploadService,
+  AuthService,
 }
